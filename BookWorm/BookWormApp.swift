ad26462508b19327646e2f43672273e89cab8b69
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-21 15:42:29 -0700

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Andres Gutierrez on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
