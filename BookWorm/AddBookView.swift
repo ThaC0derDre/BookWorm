f2833c87d3ce30599fc15554fc81c3a30d6ffa46
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-26 13:06:05 -0700

    Set up AddBookView with initial UI and functionality of saving to coreData

---
+++ BookWorm/AddBookView.swift
@@ -0,0 +1,70 @@
+//
+//  AddBookView.swift
+//  BookWorm
+//
+//  Created by Andres Gutierrez on 4/26/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var title    = ""
+    @State private var author   = ""
+    @State private var review   = ""
+    @State private var rating   = 3
+    @State private var genre    = ""
+    
+    let genres    = ["Fantasy", "Romance", "Poetry", "Horror"]
+    
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Name of Book", text: $title)
+                    TextField("Name of Author", text: $author)
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id:\.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section{
+                    TextEditor(text: $review)
+                    Picker("Rating", selection: $rating) {
+                        ForEach(1..<6) {
+                            Text(String($0))
+                        }
+                    }
+                } header: {
+                    Text("Write a review")
+                }
+                Section{
+                    Button("Save"){
+                        //add book
+                        let books       = Book(context: moc)
+                        books.id        = UUID()
+                        books.title     = title
+                        books.genre     = genre
+                        books.author    = author
+                        books.review    = review
+                        books.rating    = Int16(rating)
+                        
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -8,29 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     var body: some View {
-        List(students){ student in
-            Text(student.name ?? "?")
-        }
-        
-        Button("Add"){
-            let firstName   = ["Estevan", "Joshua", "Josh", "Dre"]
-            let lastName    = ["Gutierrez", "Hinojosa", "Ortiz", "Jensen"]
-            
-            let chosenFirstName = firstName.randomElement()!
-            let chosenLastName  = lastName.randomElement()!
-            
-            let student = Student(context: moc)
-            student.id  = UUID()
-            student.name    = "\(chosenFirstName) \(chosenLastName)"
-            
-            try? moc.save()
-            
+        Text("Heller")
         }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
