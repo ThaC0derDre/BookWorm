09f463da8b00d981c696ac9aec18229bfc0e71a4
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-27 11:17:28 -0700

    Add form validation for Button in AddBookV Add date property in Model and DetailV Low rated books font red

--- BookWorm/AddBookView.swift
+++ BookWorm/AddBookView.swift
@@ -11,11 +11,13 @@ import CoreData
 struct AddBookView: View {
     @Environment(\.managedObjectContext) var moc
     @Environment(\.dismiss) var dismiss
-    @State private var title    = ""
-    @State private var author   = ""
-    @State private var review   = ""
-    @State private var rating   = 3
-    @State private var genre    = ""
+    @State private var title        = ""
+    @State private var author       = ""
+    @State private var review       = ""
+    @State private var rating       = 3
+    @State private var genre        = ""
+    @State private var emptyField   = false
+    @State private var date         = Date.now.formatted(date: .abbreviated, time: .shortened)
     let genres    = ["Fantasy", "Romance", "Poetry", "Horror", "Kids", "Thriller", "Mystery"]
     
     
@@ -38,27 +40,45 @@ struct AddBookView: View {
                 } header: {
                     Text("Write a review")
                 }
+                
                 Section{
                     Button("Save"){
                         //add book
                         let books       = Book(context: moc)
                         books.id        = UUID()
+                        books.date      = date
                         books.title     = title
                         books.genre     = genre
                         books.author    = author
-                        books.review    = review
                         books.rating    = Int16(rating)
                         
-                        try? moc.save()
-                        dismiss()
+                        if review.count == 0 {
+                            review = "No review"
+                        } else {
+                        books.review    = review
+                        }
                         
+                        try? moc.save()
+                        
+                        dismiss()
                     }
+                    .disabled(completedFields())
                 }
             }
             .navigationTitle("Add Book")
         }
     }
+    
+    
+    func completedFields()-> Bool {
+        if title.count == 0 || genre == "" || author.count == 0 {
+            return true
+        }else {
+        return false
+        }
+    }
 }
+
 
 struct AddBookView_Previews: PreviewProvider {
     static var previews: some View {
--- BookWorm/DetailView.swift
+++ BookWorm/DetailView.swift
@@ -20,6 +20,7 @@ struct DetailView: View {
                 Image(book.genre ?? "Fantasy")
                     .resizable()
                     .scaledToFit()
+                
                 Text(book.genre?.uppercased() ?? "FANTASY")
                     .font(.caption)
                     .fontWeight(.black)
@@ -29,6 +30,7 @@ struct DetailView: View {
                     .clipShape(Capsule())
                     .offset(x: -5, y: -5)
             }
+            
             Text(book.author ?? "Unknown")
                 .font(.title)
                 .foregroundColor(.secondary)
@@ -36,20 +38,29 @@ struct DetailView: View {
             Text(book.review ?? "No Review")
                 .padding()
             
+            ZStack(alignment: .bottomTrailing) {
+                Text(book.date ?? "")
+                    .font(.footnote)
+                    .padding(.bottom, 5)
+            }
+            .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+            
             RatingView(rating: .constant(Int(book.rating)))
                 .font(.largeTitle)
         }
         .navigationTitle(book.title ?? "Unknown")
         .navigationBarTitleDisplayMode(.inline)
+        
         .alert("Delete Book?", isPresented: $showDeleteAlert){
             Button("Delete", role: .destructive) {
                 deleteBook()
             }
+            
             Button("Cancel", role: .cancel) { }
-
         } message: {
             Text("Are you sure?")
         }
+        
         .toolbar {
             Button{
                 showDeleteAlert = true
